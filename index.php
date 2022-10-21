99c4608fab5a40bd13495e5495039762e661e701
Caio Rebert <caiorebert070802.cr@gmail.com>
2022-10-21 20:16:09 -0300

    algum commit

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php 
 
+    if (isset($_GET['op'])) {
+        switch ($_GET['op']) {
+            case 'ins':
+                
+                break;
+            case 'sele':
+                break;
+            default:
+                # code...
+                break;
+        }
+    }
     if (isset($_GET['email'])) {
         $email = $_GET['email'];
         print_r("Olá $email, como vc está?<br>");
         print_r("Conectando...");
         if ($conn = pg_connect(getenv("DATABASE_URL"))) {
             $result_db = pg_query($conn, "SELECT * FROM public.mensagens WHERE email LIKE '%$email%'");
-            foreach ($result_db as $key => $value) {
-                print_r($value);
+            while($row = pg_fetch_row($result_db)) {
+                print_r($row);
             }
         } 
     }
