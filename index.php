269bdc38cc3dae7d9902772bda995d76b4ac0131
Caio Rebert <caiorebert070802.cr@gmail.com>
2022-10-20 21:17:06 -0300

    four commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     if (isset($_GET['email'])) {
         $email = $_GET['email'];
         print_r("Olá $email, como vc está?\n");
-        print_r(getenv("URL_DB"));
+        print_r(getenv("DATABASE_URL"));
     }
     print_r("deu bom");
 
