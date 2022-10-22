335772f91b266724e707d2aa1a6ac97fa1bedb84
Caio Rebert <caiorebert070802.cr@gmail.com>
2022-10-22 10:36:55 -0300

    algum commit ae

--- index.php
+++ index.php
@@ -15,14 +15,17 @@
     if (isset($_GET['email'])) {
         $email = $_GET['email'];
         print_r("Olá $email, como vc está?<br>");
-        print_r("Conectando...");
+        print_r("Conectando...<br>");
         if ($conn = pg_connect(getenv("DATABASE_URL"))) {
-            $result_db = pg_query($conn, "SELECT * FROM public.mensagens WHERE email LIKE '%$email%'");
-            while($row = pg_fetch_row($result_db)) {
-                print_r($row);
+            print_r("deu bom");
+            if ($result_db = pg_query($conn, "SELECT * FROM public.mensagens WHERE email LIKE '%$email%'")) {
+                while($row = pg_fetch_row($result_db)) {
+                    print_r($row);
+                }
+            } else {
+                pg_result_error($result_db);
             }
         } 
     }
-    print_r("deu bom");
 
 ?>
