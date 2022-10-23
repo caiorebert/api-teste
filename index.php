433ec3fa3db2805ab628ac18d6396a4d4c15a450
Caio Rebert <caiorebert070802.cr@gmail.com>
2022-10-23 13:12:06 -0300

    commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
                 2 => $_POST['message']
             ];
             if (getenv("DATABASE_URL")!="") {
-                if ($conn = pg_connect(getenv("DATABASE_URL")) {
+                if ($conn = pg_connect(getenv("DATABASE_URL"))) {
                     if ($result_db = pg_query($conn, "INSERT INTO public.mensagens (name, email, message) VALUES ('{$array[0]}', '{$array[1]}', '{$array[2]}') ")) {
                         print_r("adicionado!");
                     } else {
@@ -23,34 +23,7 @@
                     }
                 }
             }
-            
         }
-    }
-    if (isset($_GET['op'])) {
-        switch ($_GET['op']) {
-            case 'ins':
-                
-                break;
-            case 'sele':
-                break;
-            default:
-                # code...
-                break;
-        }
-    }
-    if (isset($_GET['email'])) {
-        $email = $_GET['email'];
-        print_r("Olá $email, como vc está?<br>");
-        print_r("Conectando...<br>");
-        if ($conn = pg_connect(getenv("DATABASE_URL"))) {
-            if ($result_db = pg_query($conn, "SELECT * FROM public.mensagens WHERE email LIKE '%$email%'")) {
-                while($row = pg_fetch_row($result_db)) {
-                    print_r($row);
-                }
-            } else {
-                pg_result_error($result_db);
-            }
-        } 
     }
 
 ?>
