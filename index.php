4869b5b7cb8cf599de3ab7d195dbba946de9d114
Caio Rebert <caiorebert070802.cr@gmail.com>
2022-10-23 13:14:47 -0300

    commit

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
                 2 => $_POST['message']
             ];
             if (getenv("DATABASE_URL")!="") {
-                if ($conn = pg_connect(getenv("DATABASE_URL"))) {
-                    if ($result_db = pg_query($conn, "INSERT INTO public.mensagens (name, email, message) VALUES ('{$array[0]}', '{$array[1]}', '{$array[2]}') ")) {
-                        print_r("adicionado!");
-                    } else {
-                        pg_result_error($result_db);
-                    }
-                }
+                // if ($conn = pg_connect(getenv("DATABASE_URL"))) {
+                //     if ($result_db = pg_query($conn, "INSERT INTO public.mensagens (name, email, message) VALUES ('{$array[0]}', '{$array[1]}', '{$array[2]}') ")) {
+                //         print_r("adicionado!");
+                //     } else {
+                //         pg_result_error($result_db);
+                //     }
+                // }
             } else {
                 if ($conn = pg_connect("host=localhost port=5432 dbname=public user=postgres password=1234")) {
                     if ($result_db = pg_query($conn, "INSERT INTO public.mensagens (name, email, message) VALUES ('{$array[0]}', '{$array[1]}', '{$array[2]}') ")) {
