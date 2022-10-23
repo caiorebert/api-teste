ebd8236769f626c84188647e4afddb21d641fb8c
Caio Rebert <caiorebert070802.cr@gmail.com>
2022-10-23 12:33:53 -0300

    commit

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php 
     if (isset($_POST)) {
         if (isset($_POST['email']) && isset($_POST['message']) && isset($_POST['name'])) {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $message = $_POST['message'];
-            if ($conn = pg_connect(getenv("DATABASE_URL"))) {
-                if ($result_db = pg_insert($conn, "INSERT INTO public.mensagens (id, name, email, message) VALUES (2, $name, $email, $message)")) {
-                    return '{
-                        "message": "Adicionado!"
-                    }';
+            $array = [
+                0 => $_POST['name'],
+                1 =>$_POST['email'],
+                2 => $_POST['message']
+            ];
+            if ($conn = pg_connect("host=localhost port=5432 dbname=public user=postgres password=1234")) {
+                if ($result_db = pg_query($conn, "INSERT INTO public.mensagens (name, email, message) VALUES ('{$array[0]}', '{$array[1]}', '{$array[2]}') ")) {
+                    print_r("adicionado!");
                 } else {
                     pg_result_error($result_db);
                 }
